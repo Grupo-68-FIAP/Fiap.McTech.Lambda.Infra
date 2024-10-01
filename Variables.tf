d6cab416393bcce1cccb9dd105a33e91fd209f05
Felipe Tomm <felipe.tomm@gmail.com>
2024-10-01 00:43:14 -0300

    format file

--- Variables.tf
+++ Variables.tf
@@ -1,22 +1,22 @@
 variable "aws_region" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "Define a região padrão da AWS onde os recursos serão provisionados."
 }
 
 variable "project_name" {
-  type = string
-  default = "MechTechApi"
+  type        = string
+  default     = "MechTechLambda"
   description = "Especifica o nome do projeto"
 }
 
 variable "accountIdVoclabs" {
-  type = string
+  type        = string
   description = "Representa o ID da conta da AWS para o laboratório vocacional. Esta variável é obrigatória."
 }
 
 variable "accessConfig" {
-  type = string
-  default = "API_AND_CONFIG_MAP"
+  type        = string
+  default     = "API_AND_CONFIG_MAP"
   description = "Especifica a configuração de acesso desejada, incluindo tanto a API quanto um mapa de configuração."
 }
